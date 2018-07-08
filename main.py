ce9520afe5846935e0c8627d47ca6872ad0581ab
James Robertson <jamesurobertson@gmail.com>
2018-07-08 00:21:11 -0500

    pipe continuancy

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import sys, pygame, random
 pygame.init()
 
+# Constants
+BACKGROUND = "background.png"
+BIRD = "james.png"
+PIPE = "pipe.bmp"
+FPS = 60
+WINDOW_SIZE = WIDTH, HEIGHT = 480, 480
+SPEED_BOUNCE = -10
+SPEED_MAX = 25
+GRAVITY = 0.65
+clock = pygame.time.Clock()
+
+# Variables
+speed = [0, 0]
+started = False
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+# background scrolling
+bkgd = pygame.image.load(BACKGROUND).convert()
+bkgdx = 0
+
 def handle_event():
     global started
     for event in pygame.event.get():
@@ -18,25 +38,6 @@ def load_bird(img):
     rect.bottom = (HEIGHT / 2) + (rect.height / 2)
     return bird, rect
 
-# Constants
-BACKGROUND = "background.png"
-BIRD = "james.png"
-FPS = 60
-WINDOW_SIZE = WIDTH, HEIGHT = 480, 480
-SPEED_BOUNCE = -10
-SPEED_MAX = 25
-GRAVITY = 0.65
-clock = pygame.time.Clock()
-
-# Variables
-speed = [0, 0]
-started = False
-screen = pygame.display.set_mode(WINDOW_SIZE)
-
-# background scrolling
-bkgd = pygame.image.load(BACKGROUND).convert()
-bkgdx = 0
-
 # draw pipes and set initial positions
 def load_pipe(img):
     pipeUpper = pygame.image.load(img)
@@ -45,16 +46,28 @@ def load_pipe(img):
     pipeLowerRect = pipeLower.get_rect()
     pipeUpperRect.left = WIDTH + (WIDTH / 3)
     pipeLowerRect.left = WIDTH + (WIDTH / 3)
-    pipeUpperRect.bottom = random.randrange(30 , 322)
+    pipeUpperRect.bottom = random.randrange(30 , 300)
     # space between pipes
-    pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
+    pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2)
     return pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect
 
-james, jamesrect = load_bird(BIRD)
+def load_pipe2(img):
+    pipeUpper2 = pygame.image.load(img)
+    pipeLower2 = pygame.image.load(img)
+    pipeUpperRect2 = pipeUpper.get_rect()
+    pipeLowerRect2 = pipeLower.get_rect()
+    pipeUpperRect2.left = pipeUpperRect.left + (WIDTH * .73)
+    pipeLowerRect2.left = pipeLowerRect.left + (WIDTH * .73)
 
-# pipe scrolling
-pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect = load_pipe("pipe.bmp")
-pipeUpper2, pipeUpperRect2, pipeLower2, pipeLowerRect2 = load_pipe("pipe.bmp")
+    pipeUpperRect2.bottom = random.randrange(30 , 300)
+    # space between pipes
+    pipeLowerRect2.top = pipeUpperRect2.bottom + (jamesrect.height * 2)
+    return pipeUpper2, pipeUpperRect2, pipeLower2, pipeLowerRect2
+
+# load objects
+james, jamesrect = load_bird(BIRD)
+pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect = load_pipe(PIPE)
+pipeUpper2, pipeUpperRect2, pipeLower2, pipeLowerRect2 = load_pipe2(PIPE)
 
 
 # main loop
@@ -63,55 +76,46 @@ while True:
 
     # scrolling backgroundimage
     rel_x = bkgdx % bkgd.get_rect().width
-	
-    screen.blit(bkgd, (rel_x - bkgd.get_rect().width, 0))
     if rel_x < WIDTH:
         screen.blit(bkgd, (rel_x, 0))
+
     bkgdx += -2
 
     if started:    
         # replacing the pipes
         if pipeUpperRect.left < -pipeUpperRect.width:
-            pipeUpperRect.left = WIDTH + (WIDTH / 3)
-            pipeLowerRect.left = WIDTH + (WIDTH / 3)
-            pipeUpperRect.bottom = random.randrange(30 , 322)
-            pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
-        if pipeUpperRect2.left < -pipeUpperRect.width:
-            pipeUpperRect2.left = WIDTH + (WIDTH / 3)
-            pipeLowerRect2.left = WIDTH + (WIDTH / 3)
-            pipeUpperRect2.bottom = random.randrange(30 , 322)
-            pipeLowerRect2.top = pipeUpperRect2.bottom + (jamesrect.height * 2.5)
-        
+            pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect = load_pipe(PIPE)
+        if pipeUpperRect2.left < -pipeUpperRect2.width:
+            pipeUpper2, pipeUpperRect2, pipeLower2, pipeLowerRect2 = load_pipe2(PIPE)
+
         pipeUpperRect.left += -5
         pipeLowerRect.left += -5
+        pipeUpperRect2.left += -5
+        pipeLowerRect2.left += -5
+
 
         # Flipping the sign of speed makes it change direction.
-        # This combined with line 28 gives a bouncing effect.
         if jamesrect.bottom > HEIGHT:
             speed[1] = 0
             started = False
-            jamesrect.left = (WIDTH / 3) - (jamesrect.width / 2)
-            jamesrect.bottom = (HEIGHT / 2) + (jamesrect.height / 2)
-            pipeUpperRect.left = WIDTH + (WIDTH / 3)
-            pipeLowerRect.left = WIDTH + (WIDTH / 3)
-            pipeUpperRect.bottom = random.randrange(30 , 322)
-            pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
-            pipeUpperRect2.left = WIDTH + (WIDTH / 3)
-            pipeLowerRect2.left = WIDTH + (WIDTH / 3)
-            pipeUpperRect2.bottom = random.randrange(30 , 322)
-            pipeLowerRect2.top = pipeUpperRect2.bottom + (jamesrect.height * 2.5)
+            pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect = load_pipe(PIPE)
+            pipeUpper2, pipeUpperRect2, pipeLower2, pipeLowerRect2 = load_pipe2(PIPE)
+            james, jamesrect = load_bird(BIRD)
+
 
         # Accelerate up to speed_max
         if speed[1] < SPEED_MAX:
             speed[1] += GRAVITY
 
         jamesrect = jamesrect.move(speed)
-        
+
+    screen.blit(bkgd, (rel_x - bkgd.get_rect().width, 0))
     screen.blit(pipeUpper, pipeUpperRect)
     screen.blit(pipeLower, pipeLowerRect)
     screen.blit(pipeUpper2, pipeUpperRect2)
     screen.blit(pipeLower2, pipeLowerRect2)
-    screen.blit(james, jamesrect)
 
+    screen.blit(james, jamesrect)
+        
     pygame.display.flip()
     clock.tick(FPS)
