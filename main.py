7e51587d42119a967184ff8276431f0cdcfd03fa
Collin Brittain <cbrit@MBPro.local>
2018-07-07 21:57:01 -0500

    Fixed bugs

--- main.py
+++ main.py
@@ -50,26 +50,16 @@ def load_pipe(img):
     pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
     return pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect
 
+james, jamesrect = load_bird(BIRD)
+
 # pipe scrolling
 pipeUpper, pipeUpperRect, pipeLower, pipeLowerRect = load_pipe("pipe.bmp")
 pipeUpper2, pipeUpperRect2, pipeLower2, pipeLowerRect2 = load_pipe("pipe.bmp")
 
-james, jamesrect = load_bird(BIRD)
 
 # main loop
 while True:
     handle_event()
-
-    # Flipping the sign of speed makes it change direction.
-    # This combined with line 28 gives a bouncing effect.
-    if jamesrect.bottom > HEIGHT:
-        speed[1] = 0
-
-    # Accelerate up to speed_max
-    if speed[1] < SPEED_MAX:
-        speed[1] += GRAVITY
-
-    jamesrect = jamesrect.move(speed)
 
     # scrolling backgroundimage
     rel_x = bkgdx % bkgd.get_rect().width
@@ -78,23 +68,23 @@ while True:
     if rel_x < WIDTH:
         screen.blit(bkgd, (rel_x, 0))
     bkgdx += -2
-    
-    # replacing the pipes
-    if pipeUpperRect.left < -pipeUpperRect.width:
-        pipeUpperRect.left = WIDTH + (WIDTH / 3)
-        pipeLowerRect.left = WIDTH + (WIDTH / 3)
-        pipeUpperRect.bottom = random.randrange(30 , 322)
-        pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
-    if pipeUpperRect2.left < -pipeUpperRect.width:
-        pipeUpperRect2.left = WIDTH + (WIDTH / 3)
-        pipeLowerRect2.left = WIDTH + (WIDTH / 3)
-        pipeUpperRect2.bottom = random.randrange(30 , 322)
-        pipeLowerRect2.top = pipeUpperRect2.bottom + (jamesrect.height * 2.5)
-    
-    pipeUpperRect.left += -5
-    pipeLowerRect.left += -5 
 
-    if started:
+    if started:    
+        # replacing the pipes
+        if pipeUpperRect.left < -pipeUpperRect.width:
+            pipeUpperRect.left = WIDTH + (WIDTH / 3)
+            pipeLowerRect.left = WIDTH + (WIDTH / 3)
+            pipeUpperRect.bottom = random.randrange(30 , 322)
+            pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
+        if pipeUpperRect2.left < -pipeUpperRect.width:
+            pipeUpperRect2.left = WIDTH + (WIDTH / 3)
+            pipeLowerRect2.left = WIDTH + (WIDTH / 3)
+            pipeUpperRect2.bottom = random.randrange(30 , 322)
+            pipeLowerRect2.top = pipeUpperRect2.bottom + (jamesrect.height * 2.5)
+        
+        pipeUpperRect.left += -5
+        pipeLowerRect.left += -5
+
         # Flipping the sign of speed makes it change direction.
         # This combined with line 28 gives a bouncing effect.
         if jamesrect.bottom > HEIGHT:
@@ -102,6 +92,14 @@ while True:
             started = False
             jamesrect.left = (WIDTH / 3) - (jamesrect.width / 2)
             jamesrect.bottom = (HEIGHT / 2) + (jamesrect.height / 2)
+            pipeUpperRect.left = WIDTH + (WIDTH / 3)
+            pipeLowerRect.left = WIDTH + (WIDTH / 3)
+            pipeUpperRect.bottom = random.randrange(30 , 322)
+            pipeLowerRect.top = pipeUpperRect.bottom + (jamesrect.height * 2.5)
+            pipeUpperRect2.left = WIDTH + (WIDTH / 3)
+            pipeLowerRect2.left = WIDTH + (WIDTH / 3)
+            pipeUpperRect2.bottom = random.randrange(30 , 322)
+            pipeLowerRect2.top = pipeUpperRect2.bottom + (jamesrect.height * 2.5)
 
         # Accelerate up to speed_max
         if speed[1] < SPEED_MAX:
@@ -109,11 +107,11 @@ while True:
 
         jamesrect = jamesrect.move(speed)
         
-    screen.blit(james, jamesrect)
     screen.blit(pipeUpper, pipeUpperRect)
     screen.blit(pipeLower, pipeLowerRect)
     screen.blit(pipeUpper2, pipeUpperRect2)
     screen.blit(pipeLower2, pipeLowerRect2)
+    screen.blit(james, jamesrect)
 
     pygame.display.flip()
     clock.tick(FPS)
